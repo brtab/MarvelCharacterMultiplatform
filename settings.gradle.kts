890dd4aa9fbe380c87104a1af096e03b807d33e4
Brian <brtabarez@alumno.unlam.edu.ar>
2022-05-28 13:28:40 -0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MarvelCharacterMultiplatform"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/marvelcharactermultiplatform/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.marvelcharactermultiplatform
+
+expect class Platform() {
+    val platform: String
+}
